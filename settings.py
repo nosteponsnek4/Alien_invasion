07414eeb6f4b4c6b8d9bed558328d6a9cfd9e175
nosteponsnek4 <116190287+nosteponsnek4@users.noreply.github.com>
2022-10-26 11:24:19 -0400

    cleaned up formatting errors

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-class Settings():
+class Settings:
     def __init__(self):
         self.screen_width = 400
         self.screen_height = 300
--- ship.py
+++ ship.py
@@ -1,6 +1,7 @@
 import pygame
 
-class Ship():
+
+class Ship:
     def __init__(self, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
